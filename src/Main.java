61b876c8865d47ae0f681a8baf6943c4e98a682c
Scott Parrish <slparrish@gmail.com>
2025-11-03 23:29:48 -0500

    finished example/test

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,11 @@ public class Main {
         RegularPolygon poly1 = new RegularPolygon();
         RegularPolygon poly2 = new RegularPolygon(6, 4);
         RegularPolygon poly3 = new RegularPolygon(10, 4, 5.6, 7.8);
-        System.out.println(poly1.getArea());
-        System.out.println(poly2.getPerimeter());
-        System.out.println(poly3.getN());
+        System.out.println("The area of the first object is: " + poly1.getArea());
+        System.out.println("The perimeter of the first object is: " + poly1.getPerimeter());
+        System.out.println("The area of the second object is: " + poly2.getArea());
+        System.out.println("The perimeter of the second object is: " + poly2.getPerimeter());
+        System.out.println("The area of the third object is: " + poly3.getArea());
+        System.out.println("The perimeter of the third object is: " + poly3.getPerimeter());
     }
 }
