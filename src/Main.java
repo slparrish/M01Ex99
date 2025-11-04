3671b8dffd0b97f6dd2c9d0817209153973a31b8
Scott Parrish <slparrish@gmail.com>
2025-11-03 21:42:53 -0500

    added constructors

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Scott L. Parrish 11/3/2025
+// SDEV 200 Module 1 Exercise 9.9
+// Main class to test and show the use of RegularPolygon class.
+//
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        RegularPolygon poly1 = new RegularPolygon();
+        RegularPolygon poly2 = new RegularPolygon(6, 4);
+        RegularPolygon poly3 = new RegularPolygon(10, 4, 5.6, 7.8);
+        System.out.println(poly1);
+        System.out.println(poly2);
+        System.out.println(poly3);
     }
 }
--- src/RegularPolygon.java
+++ src/RegularPolygon.java
@@ -1,4 +1,25 @@
-﻿package PACKAGE_NAME;
-
+// Scott L. Parrish 11/3/2025
+// SDEV 200 Module 1 Exercise 9.9
+// RegularPolygon Class to represent n-sided regular polygons.
+//
 public class RegularPolygon {
+
+    private int n = 3;
+    private double side = 1;
+    private double x = 0;
+    private double y = 0;
+
+
+    public RegularPolygon() {
+    }
+    public RegularPolygon(int numberOfSides, double sideLength) {
+        n = numberOfSides;
+        side = sideLength;
+    }
+    public RegularPolygon(int numberOfSides, double sideLength, double xCoord, double yCoord) {
+        n = numberOfSides;
+        side = sideLength;
+        x = xCoord;
+        y = yCoord;
+    }
 }
